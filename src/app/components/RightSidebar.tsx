463e688de36eeaee70fad5223fc5adbe573df658
Cyber Phantom <inam123451@gmail.com>
2023-11-18 22:34:56 +0530

    Add Sidebar components

---
+++ src/app/components/RightSidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+  <div className="bg-text flex justify-between vertical-writing-lr w-16 lg:w-20 py-5">
+      <div className="self-center text-background">CONTACT</div>
+      <div className="self-center text-background">=</div>
+      <div className="self-center text-background">PHANTOM</div>
+    </div>
+  )
+}
+
+export default RightSidebar
