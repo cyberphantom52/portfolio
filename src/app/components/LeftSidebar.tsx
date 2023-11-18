0b3c2d38f3e3832c1dbb1881420e73c237c31d2f
Cyber Phantom <inam123451@gmail.com>
2023-11-18 22:34:56 +0530

    Add Sidebar components

---
+++ src/app/components/LeftSidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LeftSidebar = () => {
+  return (
+    <div className="bg-[#fffff5] flex justify-between vertical-writing-lr w-16 lg:w-20 py-5">
+      <span className="self-center rotate-180">HOME</span>
+      <span className="self-center rotate-180">W</span>
+      <span className="self-center rotate-180">GitHub.GitLab</span>
+    </div>
+  )
+}
+
+export default LeftSidebar
---
+++ src/app/components/RightSidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+  <div className="bg-[#fffff5] flex justify-between vertical-writing-lr w-16 lg:w-20 py-5">
+      <div className="self-center">CONTACT</div>
+      <div className="self-center">=</div>
+      <div className="self-center">PHANTOM</div>
+    </div>
+  )
+}
+
+export default RightSidebar
