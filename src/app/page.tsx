8ce3ca23a42a9ce19859bd4921f2f4971e7eac2b
Cyber Phantom <inam123451@gmail.com>
2023-11-19 02:02:27 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className='transition-{text} duration-500  flex w-screen bg-background sm: text-5xl md:text-6xl lg:text-8xl text-text justify-center'>
-      <div className="flex flex-col w-3/4 h-3/4 align-middle self-center">
-        <div className='font-bold'>CYBER</div>
+    <main className='flex w-screen bg-background justify-center transition-{text} duration-500 sm:text-5xl md:text-6xl lg:text-8xl text-text'>
+      <div className="flex flex-col w-3/4 h-3/4 self-center">
+        <span className='font-bold'>CYBER</span>
         <div className='flex h-full bg-accent'>
           <div className='flex flex-col h-full w-1/12 mr-2'>
             <div className='bg-[#f0d14b] h-1/2 w-full'></div>
@@ -14,7 +14,10 @@ export default function Home() {
           </div>
           <div className='bg-[#33856e] h-full w-1/4'></div>
         </div>
-        <div className='font-bold text-accent self-end'>PHANTOM</div>
+        <div className="flex justify-between">
+          <span className='transition-{text} duration-500 font-bold text-[#4c4f48] text-xs md:text-3xl lg:text-3xl self-center'>Will You Find Out?</span>
+          <span className='font-bold text-accent'>PHANTOM</span>
+        </div>
       </div>
     </main>
   )
