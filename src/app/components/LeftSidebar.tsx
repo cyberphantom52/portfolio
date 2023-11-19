f0d1e0242d13be5cb05fd2aa0bcaa4d04b8c2e5c
Cyber Phantom <inam123451@gmail.com>
2023-11-19 15:05:19 +0530

    Make sure sidebars are always on top

--- src/app/components/LeftSidebar.tsx
+++ src/app/components/LeftSidebar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LeftSidebar = () => {
   return (
-    <div className="bg-text flex justify-between vertical-writing-lr w-16 lg:w-20 py-5">
+    <div className="bg-text flex justify-between vertical-writing-lr w-16 lg:w-20 py-5 z-10">
       <span className="self-center rotate-180 text-background">HOME</span>
       <span className="self-center rotate-180 text-background">W</span>
       <span className="self-center rotate-180 text-background">GitHub.GitLab</span>
