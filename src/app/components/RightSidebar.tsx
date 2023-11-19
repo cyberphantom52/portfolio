e0ac8a34d4575c39dfc44980aec103805e9ca412
Cyber Phantom <inam123451@gmail.com>
2023-11-19 15:05:19 +0530

    Make sure sidebars are always on top

--- src/app/components/RightSidebar.tsx
+++ src/app/components/RightSidebar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightSidebar = () => {
   return (
-  <div className="bg-text flex justify-between vertical-writing-lr w-16 lg:w-20 py-5">
+  <div className="bg-text flex justify-between vertical-writing-lr w-16 lg:w-20 py-5 z-10">
       <div className="self-center text-background">CONTACT</div>
       <div className="self-center text-background">=</div>
       <div className="self-center text-background">PHANTOM</div>
