44f8c447a4443ba7bc6e2265696138ce23d93692
Cyber Phantom <inam123451@gmail.com>
2023-11-22 00:36:25 +0530

    Setup framer-motion

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="flex bg-[#33856e] w-full h-full place-content-center">
+      <span className="self-center font-bold text-9xl">HELLLLOOO!</span>
+    </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import LeftSidebar from './components/LeftSidebar'
-import RightSidebar from './components/RightSidebar'
-import { url } from 'inspector'
-
+import LayoutWrapper from './components/LayoutWrapper';
 
 export const metadata: Metadata = {
   title: 'Website',
@@ -15,12 +12,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="en">
-      <body className={`flex justify-between h-screen`}>
-          <LeftSidebar />
-          {children}
-          <RightSidebar />
+      <body>
+        <LayoutWrapper>{children}</LayoutWrapper>
       </body>
     </html>
   )
