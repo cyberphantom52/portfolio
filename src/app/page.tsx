e60bd5e417deaad7214e94bb13a2af2151e953d8
Cyber Phantom <inam123451@gmail.com>
2023-11-24 01:11:36 +0530

    Add Page Transision Animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <main className='flex w-screen bg-background justify-center transition-{text} duration-500 sm:text-5xl md:text-6xl lg:text-8xl text-text'>
       <motion.div className="flex flex-col w-3/4 h-3/4 self-center"
-        initial={{ rotate: 90, scale: 4 }}
+        initial={{ rotate: 90, scale: 5 }}
         animate={{ rotate: 0, scale: 1}}
-        exit={{ rotate: 90, scale: 4 }}
-        transition={{ duration: 1 }}
+        exit={{ rotate: 90, scale: 5 }}
+        transition={{ duration: 1.66, ease: [0.16, 1, 0.3, 1]}}
         >
         <span className='font-bold'>CYBER</span>
         <HeroBanner />
