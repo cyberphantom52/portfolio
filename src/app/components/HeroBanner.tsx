95689d4c5425492b24417e8612591e6464d42b0a
Cyber Phantom <inam123451@gmail.com>
2023-12-04 16:25:26 +0530

    WIP: HeroLinks

--- src/app/components/HeroBanner.tsx
+++ src/app/components/HeroBanner.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PageOpener from './PageOpener';
+import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
@@ -12,7 +13,10 @@ const HeroBanner = () => {
           <div className='bg-[#33856e] h-full w-1/4 self-end' />
         </div>
       </div>
-      <div className='flex bg-[#33856e] h-full w-[24%] place-content-end'>
+      <div className='flex bg-[#33856e] h-full w-[24%] place-content-end relative'>
+        <Link href="/about" className='flex w-full h-full absolute'>
+          <span className='text-black self-end mb-[5%] ml-[3%] text-sm'> PROJECTS </span>
+        </Link>
         <div
           className='bg-[#f0d14b] h-full w-[18%] self-end'
           style=
@@ -23,48 +27,61 @@ const HeroBanner = () => {
           }
         />
       </div>
-      <div className='flex h-full w-[7.3%] ps-[1.7%]'>
-        <div className='bg-white h-full w-[3.48%]' />
-        <div className='bg-[#33856e] h-full w-[16.08%]' />
-        <div className='bg-white h-full w-[10.32%]' />
+      <div className='flex h-full w-[3.4%] ps-[1.7%]'>
+        <div className='bg-white h-full w-[15%]' />
+        <div className='bg-[#33856e] h-full w-[50%]' />
+        <div className='bg-white h-full w-[35%]' />
       </div>
-      <div className='bg-[#f0d14b] w-[13%] h-[80%]' />
+      <Link href="/about" className='w-[11.3%] h-full ps-[3.9%] flex flex-col'>
+        <Link href="/about" className='bg-[#f0d14b] w-full h-[80%] flex'>
+          <span className='text-black mb-[10%] ml-[10%] self-end text-sm w-full'> HORZ </span>
+        </Link>
+        <span className='text-black mt-[10%] mr-[10%] text-sm vertical-writing-rl'> VERT </span>
+      </Link>
+      <div className='h-full w-[5.6%]' />
       <PageOpener className='absolute bg-[#f0d14b] h-full w-[5.6%] left-1/2 -translate-x-1/2' />
-      <div className='relative w-[17.6%] h-full'>
-        <div className='absolute flex flex-col h-full w-[76.5%] justify-between'>
-          <div className='w-[33.3%] h-[3.6%] translate-x-[100%]'
-            style={
-              {
-                background: 'linear-gradient(90deg, #ffffff 50%, #33856e 50%)'
-              }
+      <Link href="/about" className='relative w-[17.6%] h-full'>
+        <div className='absolute w-1/4 aspect-[3/1] left-1/4'
+          style={
+            {
+              background: 'linear-gradient(90deg, #ffffff 50%, #33856e 50%)'
             }
-          />
-          <div
-            className='w-full h-[27.2%]'
-            style={
-              {
-                background: 'linear-gradient(180deg, #f0d14b 1.5%,  rgba(240, 209, 75,  0) 1.5%),  \
+          }
+        />
+        <span className='absolute text-black text-sm vertical-writing-lr mt-[38%] left-3/4 pr-[2%] -translate-x-full'> VERT </span>
+        <Link
+          href="/about"
+          className='absolute w-3/4 h-[27.2%] flex top-full -translate-y-full'
+          style={
+            {
+              background: 'linear-gradient(180deg, #f0d14b 1.5%,  rgba(240, 209, 75,  0) 1.5%),  \
                              linear-gradient(180deg, #33856e 13.5%, rgba(51,  133, 110, 0) 13.5%), \
                              linear-gradient(180deg, #f0d14b 15%,   rgba(240, 209, 75,  0) 15%),   \
                              linear-gradient(180deg, #ffffff 20%,   rgba(255, 255, 255, 0) 20%),   \
                              linear-gradient(180deg, #33856e 20%,   #33856e 20%)',
-              }
             }
-          />
-        </div>
-        <div
-          className='bg-white h-full w-1/2 translate-x-[100%]'
+          }
+        >
+          <span className='text-black text-sm ml-[5%] mt-[17%]'>DIS</span>
+        </Link>
+        <Link
+          href="/about"
+          className='absolute flex bg-white h-full w-1/2 left-1/2 justify-end'
           style=
           {
             {
               clipPath: 'polygon(100% 100%, 100% 0, 0 0, 0 16.66%, 50% 16.66%, 50% 100%)'
             }
           }
-        />
-      </div>
+        >
+          <span className='text-black text-sm vertical-writing-rl mr-[4%] mt-[8%]'> VERT </span>
+        </Link>
+      </Link>
       <div className='flex flex-col w-[16.6%] h-full justify-between'>
         <div className='flex w-[72.28%] h-[26%]'>
-          <div className='h-full bg-[#f0d14b] w-[80%]' />
+          <Link href="/about" className='flex h-full bg-[#f0d14b] w-[80%] justify-end'>
+            <span className='text-black text-sm vertical-writing-lr mr-[4%] mt-[4%]'>VERT</span>
+          </Link>
           <div className='h-full bg-[#33856e] w-[12%]' />
           <div className='h-full bg-white w-[8%]' />
         </div>
@@ -87,12 +104,19 @@ const HeroBanner = () => {
       <div className='flex flex-col w-[14%] h-full justify-end'>
         <div className='flex w-full h-[14.5%] self-end justify-between'>
           <div className='relative flex w-1/2 h-full'>
-            <div className='absolute w-[34%] h-[34%] bg-[#f0d14b] self-end -translate-x-full' />
-            <div className='w-[34%] h-[34%] bg-white self-end' />
+            <div className='absolute w-1/2 aspect-[2/1] top-full -translate-x-1/2 -translate-y-full'
+              style={
+                {
+                  background: 'linear-gradient(90deg, #f0d14b 50%, #ffffff 50%)'
+                }
+              }
+            />
           </div>
           <div className='w-1/2 h-full bg-[#f0d14b]' />
         </div>
-        <div className='w-full h-[43%] bg-[#f0d14b] self-end' />
+        <Link href="/about" className='flex w-full h-[43%] bg-[#f0d14b] self-end'>
+          <span className='text-black text-sm mt-[4%] ml-[4%]'>VERT</span>
+        </Link>
         <div className='bg-white w-full h-[8.3%]' />
       </div>
     </div >
