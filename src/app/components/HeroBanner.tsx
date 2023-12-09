b01f4b042c978a549430cb996b06b3c1469161a4
Cyber Phantom <inam123451@gmail.com>
2023-12-09 19:59:04 +0530

    Rename placeholder text for links

--- src/app/components/HeroBanner.tsx
+++ src/app/components/HeroBanner.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
-    <div className='flex h-full bg-accent relative justify-between text-sm'>
+    <div className='flex h-full bg-accent relative justify-between font-semibold'>
       {/* Left Half */}
       <div className='w-[47.5%] flex'>
         {/* First */}
@@ -19,7 +19,7 @@ const HeroBanner = () => {
         {/* Second */}
         <div className='h-full w-1/2 relative overflow-hidden'>
           <Link href='/about' className='bg-[#33856e] h-full w-full absolute'>
-            <span className='bottom-[7.5%] left-[5%] absolute'>PLACEHOLDER</span>
+            <span className='bottom-[7.5%] left-[5%] absolute'>PROJECTS</span>
           </Link>
           <div className='bg-[#f0d14b] h-full w-[18%] absolute left-full -translate-x-full'
             style={{ clipPath: 'polygon(0 100%, 0 90%, 35% 90%, 35% 84%, 83.66% 84%, 83.66% 0, 100% 0, 100% 100%)' }}
@@ -36,10 +36,10 @@ const HeroBanner = () => {
         {/* Fourth */}
         <div className='h-full w-[22%] overflow-hidden relative'>
           <Link href="/abouts" className='h-full w-full absolute'>
-            <span className='top-[82%] left-[95%] absolute -translate-x-full vertical-writing-lr'>VERT</span>
+            <span className='top-[82%] left-[95%] absolute -translate-x-full vertical-writing-lr'>ABOUT</span>
           </Link>
           <Link href="/about" className='absolute bg-[#f0d14b] w-2/3 h-4/5 left-full -translate-x-full'>
-            <span className='bottom-[7.5%] left-[5%] absolute'>HORZ</span>
+            <span className='bottom-[7.5%] left-[5%] absolute'>BLOGS</span>
           </Link>
         </div>
       </div>
@@ -60,19 +60,19 @@ const HeroBanner = () => {
                            linear-gradient(180deg, #ffffff 21%,   #33856e 21%)'
             }}
           >
-            <span className='top-[27%] left-[5%] absolute'>HORZ</span>
+            <span className='top-[27%] left-[5%] absolute'>RESUME</span>
           </Link>
           <Link href="/about" className='absolute bg-text h-full w-1/2 left-1/2'
             style={{ clipPath: 'polygon(100% 100%, 100% 0, 0 0, 0 16.66%, 50% 16.66%, 50% 100%)' }}
           >
-            <span className='absolute left-[95%] top-[2%] -translate-x-full vertical-writing-rl'>VERT</span>
+            <span className='absolute left-[95%] top-[2%] -translate-x-full vertical-writing-rl'>SKILLS</span>
           </Link>
         </div>
         {/* Second */}
         <div className='h-full w-[35%] flex flex-col justify-between'>
           <div className='flex w-[72%] h-[26%]'>
             <Link href="/about" className='relative h-full bg-[#f0d14b] w-[80%] overflow-hidden'>
-              <span className='top-[5%] left-[95%] -translate-x-full absolute vertical-writing-rl'>VERT</span>
+              <span className='top-[5%] left-[95%] -translate-x-full absolute vertical-writing-rl'>VzvNnW</span>
             </Link>
             <div className='h-full bg-[#33856e] w-[12%]' />
             <div className='h-full bg-white w-[8%]' />
@@ -94,14 +94,15 @@ const HeroBanner = () => {
           <div className='absolute w-1/4 aspect-[2/1] bottom-[50.5%] -translate-x-1/2 z-10'
             style={{ background: 'linear-gradient(90deg, #f0d14b 50%, #ffffff 50%)' }}
           />
-          <Link href="/about" className='w-full h-1/2 absolute'>
+          {/* Don't have anythig to link to here yet. */}
+          {/* <Link href="/about" className='w-full h-1/2 absolute'>
             <span className='absolute top-[5%] left-[95%] -translate-x-full vertical-writing-lr'>VERT</span>
-          </Link>
+          </Link> */}
           <div className='h-[65%] w-full self-end flex flex-col bg-text relative'
             style={{ clipPath: 'polygon(100% 0, 100% 100%, 0 100%, 0 22%, 50% 22%, 50% 0)' }}
           >
             <Link href="/about" className='h-[88%] w-full bg-[#f0d14b] relative'>
-              <span className='absolute top-[28%] left-[5%]'>HORZ</span>
+              <span className='absolute top-[28%] left-[5%]'>EXPERIENCE</span>
             </Link>
           </div>
         </div>
