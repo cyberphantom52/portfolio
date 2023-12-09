b4ffdf103a994ebdee3517f000bf7008cf5157b8
Cyber Phantom <inam123451@gmail.com>
2023-12-09 16:07:58 +0530

    Inline style text
    Applying to main class causes it to apply across each page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,18 @@ import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
-    <main className='flex w-screen bg-background justify-center text-text sm:text-5xl md:text-6xl lg:text-8xl font-bold'>
+    <main className='flex w-screen bg-background justify-center'>
       <motion.div className="flex flex-col w-3/4 h-3/4 self-center"
         initial={{ rotate: 90, scale: 5 }}
         animate={{ rotate: 0, scale: 1 }}
         exit={{ rotate: 90, scale: 5 }}
         transition={{ duration: 1.66, ease: [0.16, 1, 0.3, 1] }}
       >
-        <span className='duration-500 transition-{text}'>CYBER</span>
+        <span className='duration-500 transition-{text} text-text font-bold text-lg sm:text-4xl md:text-5xl lg:text-7xl'>CYBER</span>
         <HeroBanner />
         <div className="flex justify-between">
-          <span className='transition-{text} duration-500 text-[#4c4f48] text-xs md:text-3xl lg:text-3xl self-center'>Will You Find Out?</span>
-          <span className='transition-{text} duration-500  text-accent'>PHANTOM</span>
+          <span className='transition-{text} duration-500 text-[#4c4f48] font-bold text-lg sm:text-3xl lg:text-4xl self-center'>Will You Find Out?</span>
+          <span className='transition-{text} duration-500  text-accent font-bold text-lg sm:text-4xl md:text-5xl lg:text-7xl'>PHANTOM</span>
         </div>
       </motion.div>
     </main>
