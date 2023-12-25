fe095a6850ef01f282dcc6ed761cfe03db81f9bd
Cyber Phantom <inam123451@gmail.com>
2023-12-25 14:06:36 +0530

    Use dark theme by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
 
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
