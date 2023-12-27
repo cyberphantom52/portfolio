6da8c70a7d39e5a0d97207f3dc13f2425ff46420
Cyber Phantom <inam123451@gmail.com>
2023-12-28 00:02:33 +0530

    Update the layout of blog grid page

--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -11,10 +11,10 @@ type Props = {
 const BlogCard: React.FC<Props> = async (props) => {
 
   return (
-    <Link href={`/blogs/${props.metadata.id}`} className={`${props.cardClass} rounded-2xl flex flex-col p-6 overflow-hidden hover:scale-105 transition-transform duration-75 ease-out`}>
+    <Link href={`/blogs/${props.metadata.id}`} className={`${props.cardClass} rounded-2xl flex flex-col p-6 overflow-hidden dark:text-text hover:scale-105 transition-transform duration-75 ease-out`}>
       <h1 className={`${props.titleClass}`}>{props.metadata.title}</h1>
       <span className='italic'>{props.metadata.date}</span>
-      <hr className="hidden md:block border-t-2 border-black my-2" />
+      <hr className="hidden md:block border-t-2 border-black dark:border-white my-2" />
       <p className={`${props.descriptionClass} flex-1 overflow-hidden columns-1`}>{props.metadata.description}</p>
     </Link>
   )
