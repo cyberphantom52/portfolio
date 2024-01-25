5c9e4fbcfbc5ead101a5f2dc17a248f1f9223833
Cyber Phantom <inam123451@gmail.com>
2024-01-25 20:05:43 +0530

    WIP

--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -11,11 +11,12 @@ type Props = {
 const BlogCard: React.FC<Props> = async (props) => {
 
   return (
-    <Link href={`/blogs/${props.metadata.id}`} className={`${props.cardClass} rounded-2xl flex flex-col p-6 overflow-hidden hover:scale-105 transition-transform duration-75 ease-out`}>
-      <h1 className={`${props.titleClass}`}>{props.metadata.title}</h1>
-      <span className='italic'>{props.metadata.date}</span>
-      <hr className="hidden md:block border-t-2 border-black dark:border-white my-2" />
-      <p className={`${props.descriptionClass} flex-1 overflow-hidden columns-1`}>{props.metadata.description}</p>
+    <Link href={`/blogs/${props.metadata.id}`} className={`rounded-xl p-7 bg-black flex flex-col gap-5 overflow-hidden ${props.cardClass} hover:scale-105 transition-transform duration-75 ease-out`}>
+      <div className='flex flex-col gap-2 font-extrabold'>
+        <h2 className={`${props.titleClass}`}>{props.metadata.title}</h2>
+        <span className='text-white'>January 24, 2023</span>
+      </div>
+      <p className={`${props.descriptionClass}`}>{props.metadata.description}</p>
     </Link>
   )
 }
